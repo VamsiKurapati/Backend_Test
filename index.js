ab850cfc55ad0ae9d1dc7b7274526f3f5dd15779
VamsiKurapati <vamsikrishna.k20@iiits.in>
2025-01-10 18:15:22 +0530

    Testing Cookies

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const verifyToken = require('./utils/verifyUser.js')
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://frontend-test-kappa-sage.vercel.app",
+  origin: "http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
